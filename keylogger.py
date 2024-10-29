c006dcaf5ab24da3425c7e13cc67957d2efd033c
Fernando Antón <131510571+PRL412@users.noreply.github.com>
2024-10-29 12:37:21 -0500

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,17 +1,15 @@
 from pynput import keyboard
 
-# Lista para almacenar el texto capturado
 captura_teclas = []
-MAX_CARACTERES = 60  # Límite de caracteres por línea
-
+MAX_CARACTERES = 60  
 def procesar_tecla(tecla):
     """Procesa cada tecla presionada y la agrega a la lista captura_teclas."""
     try:
-        if tecla.char:  # Si es un carácter imprimible
+        if tecla.char:  
             captura_teclas.append(tecla.char)
-            mostrar_texto()  # Muestra el texto actualizado
+            mostrar_texto()  
     except AttributeError:
-        manejar_teclas_especiales(tecla)  # Manejo de teclas especiales
+        manejar_teclas_especiales(tecla)  
 
 def manejar_teclas_especiales(tecla):
     """Maneja las teclas especiales como espacio, enter y backspace."""
@@ -20,22 +18,19 @@ def manejar_teclas_especiales(tecla):
         mostrar_texto()
     elif tecla == keyboard.Key.enter:
         captura_teclas.append('\n')
-        mostrar_texto()  # Muestra el salto de línea
+        mostrar_texto()  
     elif tecla == keyboard.Key.backspace and captura_teclas:
-        captura_teclas.pop()  # Elimina el último carácter
-        mostrar_texto()  # Actualiza la visualización
+        captura_teclas.pop()  
+        mostrar_texto()  
 
 def mostrar_texto():
     """Muestra el texto capturado en la consola."""
-    # Convertir lista a string
     texto = ''.join(captura_teclas)
 
-    # Verificar el tamaño del texto y agregar salto de línea si es necesario
     if len(texto) >= MAX_CARACTERES and not texto.endswith('\n'):
-        texto += '\n'  # Añadir salto de línea solo si se supera el límite
+        texto += '\n'  
 
-    # Imprimir texto completo en un formato rectangular
-    print(texto)  # Muestra el texto completo
+    print(texto)  
 
 def iniciar_keylogger():
     """Inicia el listener del teclado para capturar las teclas presionadas."""
